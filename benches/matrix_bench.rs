e0e1b2edc6de318aa80dfcdb16818c77497f58f0
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2020-12-05 21:06:27 -0500

    Add new bench

--- benches/matrix_bench.rs
+++ benches/matrix_bench.rs
@@ -1,6 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-#[macro_use]
 extern crate fourai;
 
 use fourai::matrix::Matrix;
@@ -9,7 +8,8 @@ fn generate_sq(size: usize) -> Matrix<u32> {
     let mut val = vec![vec![0; size]; size];
 
     let possibles = [
-        1123, 192, 999, 2, 1, 329, 1, 223, 2, 123, 456, 123, 75, 2, 5, 8,
+        1123, 192, 999, 2, 1, 329, 1, 223, 2, 123, 456, 123, 75, 2, 5, 8, 1, 2, 3, 4, 5, 123214215,
+        123, 24, 123, 1,
     ];
 
     for i in 0..size {
@@ -22,6 +22,12 @@ fn generate_sq(size: usize) -> Matrix<u32> {
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
+    let first_512 = generate_sq(256);
+    let second_512 = first_512.clone();
+    c.bench_function("matrix 512", |b| {
+        b.iter(|| black_box(first_512.clone()) * black_box(second_512.clone()))
+    });
+
     let first_256 = generate_sq(256);
     let second_256 = first_256.clone();
     c.bench_function("matrix 256", |b| {
